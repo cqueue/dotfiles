bd20eef3279d48cbf44824fc2201dd85e1482dfb
ykmns-cq <cque.queue@googlemail.com>
2018-01-01 18:30:51 +0900

    nvim added.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,8 @@ end
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/tools/google-cloud-sdk/path.fish.inc" ]; if type source > /dev/null; source "$HOME/tools/google-cloud-sdk/path.fish.inc"; else; . "$HOME/tools/google-cloud-sdk/path.fish.inc"; end; end
 
+bass source $HOME/tools/google-cloud-sdk/path.bash.inc
+bass source $HOME/tools/google-cloud-sdk/completion.bash.inc
+
+complete --command gcloud --arguments="($HOME/tools/google-cloud-sdk/autocomplete-gcloud.py (commandline -cp))"
+
